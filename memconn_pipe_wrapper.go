60d40d412ead87ba783d37124ee456fc84a842e7
akutz <sakutz@gmail.com>
2018-04-03 10:19:13 -0500

    Replace net.OpErr addrs with wrapped addrs
    This patch updates pipeWrapper's Read, Write, SetReadDeadline, and
    SetWriteDeadline functions to check errors and if *net.OpErr, to replace
    the Addr and Source addresses with the wrapped address values.

--- memconn_pipe_wrapper.go
+++ memconn_pipe_wrapper.go
@@ -1,6 +1,9 @@
 package memconn
 
-import "net"
+import (
+	"net"
+	"time"
+)
 
 type pipeWrapper struct {
 	net.Conn
@@ -14,4 +17,54 @@ func (p pipeWrapper) LocalAddr() net.Addr {
 
 func (p pipeWrapper) RemoteAddr() net.Addr {
 	return p.remoteAddr
+}
+
+func (p *pipeWrapper) Read(b []byte) (int, error) {
+	n, err := p.Conn.Read(b)
+	if err != nil {
+		if e, ok := err.(*net.OpError); ok {
+			e.Addr = p.remoteAddr
+			e.Source = p.localAddr
+			return n, e
+		}
+		return n, err
+	}
+	return n, nil
+}
+
+func (p *pipeWrapper) Write(b []byte) (int, error) {
+	n, err := p.Conn.Write(b)
+	if err != nil {
+		if e, ok := err.(*net.OpError); ok {
+			e.Addr = p.remoteAddr
+			e.Source = p.localAddr
+			return n, e
+		}
+		return n, err
+	}
+	return n, nil
+}
+
+func (p *pipeWrapper) SetReadDeadline(t time.Time) error {
+	if err := p.Conn.SetDeadline(t); err != nil {
+		if e, ok := err.(*net.OpError); ok {
+			e.Addr = p.localAddr
+			e.Source = p.localAddr
+			return e
+		}
+		return err
+	}
+	return nil
+}
+
+func (p *pipeWrapper) SetWriteDeadline(t time.Time) error {
+	if err := p.Conn.SetWriteDeadline(t); err != nil {
+		if e, ok := err.(*net.OpError); ok {
+			e.Addr = p.localAddr
+			e.Source = p.localAddr
+			return e
+		}
+		return err
+	}
+	return nil
 }
